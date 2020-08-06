502a0e05bbdc8dac493f69f2200fd9a8df055e3b
G-Vicky <vigneshwaran2747@gmail.com>
2020-08-06 11:22:01 +0530

    Initial commit in skeleton branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Todos from "./components/todos"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
      <h1>
        React-ToDo 
      </h1>
+     <Todos />
     </div>
   );
 }
---
+++ src/components/todos.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Todos extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Todos
+            </div>
+         );
+    }
+}
+ 
+export default Todos;
